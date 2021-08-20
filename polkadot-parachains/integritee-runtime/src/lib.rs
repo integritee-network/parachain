95c8c5e13305da3d7d7dac0c3169d32dff087a1c
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-08-20 08:15:36 +0200

    [integritee-runtime] bump transaction version, as the call enum was modified

--- polkadot-parachains/integritee-runtime/src/lib.rs
+++ polkadot-parachains/integritee-runtime/src/lib.rs
@@ -88,7 +88,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_version: 10,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 1,
+	transaction_version: 2,
 };
 
 pub const MILLISECS_PER_BLOCK: u64 = 12000;
