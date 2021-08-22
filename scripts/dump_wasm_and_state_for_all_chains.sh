126c84bb4cd0b6f4562385a735000660556532b9
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-08-22 10:40:31 +0200

    [scripts] add some documentation

--- scripts/dump_wasm_and_state_for_all_chains.sh
+++ scripts/dump_wasm_and_state_for_all_chains.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 # Remark: wasm and state will be identical for different relay-chains
+
+# Helper script to generate the wasm, state and chain-spec/ -raw.json for a all chain-specs.
+#
+# Usage: ./scripts/dump_wasm_and_state_for_all_chains.sh <para-id> <collator-binary>
+#
+# Example: ./scripts/dump_wasm_state_and_spec.sh 2000
+#
+# para-id, and collator-binary is optional
+
+PARA_ID=${1:-2015}
+COLLATOR=${2:-./target/release/integritee-collator}
+DUMP_DIR=./chain_dumps
+
+mkdir -p $DUMP_DIR
+
 chainspecs=("integritee-rococo-local" \
       "integritee-rococo-local-dev" \
       "integritee-rococo" \
@@ -20,12 +35,6 @@ chainspecs=("integritee-rococo-local" \
       "shell-polkadot" \
 
       )
-
-PARA_ID=${1:-2015}
-COLLATOR=${2:-./target/release/integritee-collator}
-DUMP_DIR=./chain_dumps
-
-mkdir -p $DUMP_DIR
 
 $COLLATOR --version
 # Print array values in  lines
--- scripts/dump_wasm_state_and_spec.sh
+++ scripts/dump_wasm_state_and_spec.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-# Remark: wasm and state will be identical for different relay-chains
+
+# Helper script to generate the wasm, state and chain-spec/ -raw.json for a given chain-spec.
+#
+# Usage: ./scripts/dump_wasm_state_and_spec.sh <chain-spec> <para-id> <collator-binary>
+#
+# Example: ./scripts/dump_wasm_state_and_spec.sh shell-kusama-local-dev 2000
+#
+# chain-spec is mandatory, the rest is optional.
+
 
 CHAIN_SPEC=$1
 PARA_ID=${2:-2015}
@@ -13,6 +21,8 @@ echo ""
 
 $COLLATOR build-spec --chain ${CHAIN_SPEC} >$DUMP_DIR/${CHAIN_SPEC}.json
 $COLLATOR build-spec --chain ${CHAIN_SPEC} --raw >$DUMP_DIR/${CHAIN_SPEC}-raw.json
+
+# Overwrite the rust-default value with the $PARA_ID
 sed -i "/\"para_id\": 2015/s/2015/${PARA_ID}/" $DUMP_DIR/${CHAIN_SPEC}.json
 sed -i "/\"para_id\": 2015/s/2015/${PARA_ID}/" $DUMP_DIR/${CHAIN_SPEC}-raw.json
 
