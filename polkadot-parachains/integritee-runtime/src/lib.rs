22550cdc838a652cdf0e202fcb8831c46d629e03
Alain Brenzikofer <alain@integritee.network>
2023-08-09 13:55:23 +0100

    include teerex migrations

--- polkadot-parachains/integritee-runtime/src/lib.rs
+++ polkadot-parachains/integritee-runtime/src/lib.rs
@@ -808,7 +808,10 @@ pub type Executive = frame_executive::Executive<
 	frame_system::ChainContext<Runtime>,
 	Runtime,
 	AllPalletsWithSystem, // Solochain: AllPalletsReversedWithSystemFirst, Statemint: AllPallets. Which one to take?
-	(),
+	(
+		// can migrate from v0 or v1 to v2
+		pallet_teerex::migrations::v1::MigrateV0toV1<Runtime>,
+	),
 >;
 
 #[cfg(feature = "runtime-benchmarks")]
