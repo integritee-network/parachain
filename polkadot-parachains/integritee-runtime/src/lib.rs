9cb1e6b93255e5108e6c015c4b214b0958b42184
Alain Brenzikofer <alain@integritee.network>
2024-02-16 14:19:56 +0100

    bump versions

--- polkadot-parachains/integritee-runtime/src/lib.rs
+++ polkadot-parachains/integritee-runtime/src/lib.rs
@@ -136,7 +136,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("integritee-parachain"),
 	impl_name: create_runtime_str!("integritee-full"),
 	authoring_version: 2,
-	spec_version: 48,
+	spec_version: 49,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 6,
