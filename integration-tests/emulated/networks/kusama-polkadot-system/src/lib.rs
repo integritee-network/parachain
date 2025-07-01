c7aae5f432494d1288c6bd94462719bb6177b3b4
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-07-01 16:13:17 +0200

    fix emulated network build

--- integration-tests/emulated/networks/kusama-polkadot-system/src/lib.rs
+++ integration-tests/emulated/networks/kusama-polkadot-system/src/lib.rs
@@ -18,16 +18,18 @@ pub use asset_hub_polkadot_emulated_chain;
 pub use bridge_hub_kusama_emulated_chain;
 pub use bridge_hub_polkadot_emulated_chain;
 pub use kusama_emulated_chain;
-pub use penpal_emulated_chain;
 pub use polkadot_emulated_chain;
+pub use integritee_kusama_emulated_chain;
+pub use integritee_polkadot_emulated_chain;
 
 use asset_hub_kusama_emulated_chain::AssetHubKusama;
 use asset_hub_polkadot_emulated_chain::AssetHubPolkadot;
 use bridge_hub_kusama_emulated_chain::BridgeHubKusama;
 use bridge_hub_polkadot_emulated_chain::BridgeHubPolkadot;
 use kusama_emulated_chain::Kusama;
-use penpal_emulated_chain::{PenpalA, PenpalB};
 use polkadot_emulated_chain::Polkadot;
+use integritee_kusama_emulated_chain::IntegriteeKusama;
+use integritee_polkadot_emulated_chain::IntegriteePolkadot;
 
 // Cumulus
 use emulated_integration_tests_common::{
