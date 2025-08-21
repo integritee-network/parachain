bcc3aef6baa1753b180332325af40ffa2f39fdc0
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-08-21 13:14:58 +0200

    moaar licence headers

--- integration-tests/emulated/tests/bridges/ik-ip/src/tests/ik_to_ip_xcm.rs
+++ integration-tests/emulated/tests/bridges/ik-ip/src/tests/ik_to_ip_xcm.rs
@@ -1,3 +1,22 @@
+// Copyright 2021 Integritee AG and Supercomputing Systems AG
+// This file is part of the "Integritee parachain" and is
+// based on Cumulus from Parity Technologies (UK) Ltd.
+
+// Integritee parachain is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Cumulus is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Integritee parachain.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Tests regarding the bridge from IK to IP.
+
 use crate::{
 	tests::{
 		assert_asset_hub_kusama_message_processed, assert_asset_hub_polkadot_message_processed,
