9cfc5269ed5cb86accd7add2ea9b4ccca90e9c84
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-08-21 13:04:14 +0200

    add licence headers

--- polkadot-parachains/integritee-polkadot/src/porteer.rs
+++ polkadot-parachains/integritee-polkadot/src/porteer.rs
@@ -1,3 +1,20 @@
+// Copyright 2021 Integritee AG and Supercomputing Systems AG
+// This file is part of the "Integritee parachain" and is
+// based on Cumulus from Parity Technologies (UK) Ltd.
+
+// Integritee parachain is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Cumulus is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Integritee parachain.  If not, see <http://www.gnu.org/licenses/>.
+
 use crate::{
 	xcm_config::{AccountIdToLocation, AssetHubLocation, XcmConfig},
 	Balances, ExistentialDeposit, ParachainInfo, Porteer, Runtime,
