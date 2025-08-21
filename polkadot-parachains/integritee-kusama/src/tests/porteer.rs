0bce8bd49b9f6367cbde3a1200327c229da37cf9
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-08-21 13:09:27 +0200

    more licence headers

--- polkadot-parachains/integritee-kusama/src/tests/porteer.rs
+++ polkadot-parachains/integritee-kusama/src/tests/porteer.rs
@@ -1,4 +1,21 @@
-use crate::{IntegriteePolkadotLocation, IntegriteePolkadotSovereignAccount, RuntimeCall};
+// Copyright 2021 Integritee AG and Supercomputing Systems AG
+// This file is part of the "Integritee parachain" and is
+// based on Cumulus from Parity Technologies (UK) Ltd.
+
+// Integritee parachain is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Cumulus is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Integritee parachain.  If not, see <http://www.gnu.org/licenses/>.
+
+use crate::{porteer::IntegriteePolkadotLocation, IntegriteePolkadotSovereignAccount, RuntimeCall};
 use integritee_parachains_common::porteer::integritee_runtime_porteer_mint;
 use parity_scale_codec::{Decode, Encode};
 use xcm_executor::traits::ConvertLocation;
